8864a856b7d46c09a1685dfc590e1dca044643c5
Dgaming10 <85754917+Dgaming10@users.noreply.github.com>
2023-03-28 00:11:46 +0300

    added webservices and public folder

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ const userRouter = require("./mvc/routes/user-router");
 const accountRouter = require("./mvc/routes/my-account");
 const productRouter = require("./mvc/routes/product-router");
 const locationtRouter = require("./mvc/routes/location-router");
+var http = require('http').createServer(app); 
+var io = require('socket.io')(http);
+// //**********
+// const newLocal = require("custom-env");
+// newLocal.env(process.env.NODE_ENV, "./config");
+// //******* */
+app.use(express.static("public"));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/views/shopping-page/index.html");
+});
 
 mongoose
   .connect(
@@ -40,6 +50,27 @@ app.use("/users", userRouter);
 app.use("/account", accountRouter);
 
 const port = 3000;
-app.listen(port, () =>
-  console.log(`Server is listening: http://localhost:${port}/home-page/`)
-);
+// app.listen(port, () =>
+//   console.log(`Server is listening: http://localhost:${port}/home-page/`)
+// );
+
+io.on('connection', (socket) => {
+  console.log('new connection');
+
+  socket.on('disconnect', () => {
+      console.log('client disconnected');
+  });
+  socket.on('removeProduct', (msg) =>{
+      io.emit('removeProdutFinal',msg);
+  });
+
+  socket.on('addProduct', (msg) => {
+      socket.broadcast.emit('addProductFinal',msg);
+  });
+
+  socket.on('editProduct',(msg) => {
+      io.emit('editProductFinal',msg);
+  })
+});
+
+http.listen(3000);
